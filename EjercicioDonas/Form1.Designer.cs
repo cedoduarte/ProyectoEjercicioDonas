7e611a4e7f83f528e0e546f55ece156b791e6f6c
cedoduarte <carlosduarte.1@hotmail.com>
2015-11-25 14:24:19 -0600

    first commit

---
+++ EjercicioDonas/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace EjercicioDonas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.spinnerNumeroDeDonas = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.spinnerNumeroDeDonas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(233, 460);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // spinnerNumeroDeDonas
+            // 
+            this.spinnerNumeroDeDonas.Location = new System.Drawing.Point(107, 460);
+            this.spinnerNumeroDeDonas.Name = "spinnerNumeroDeDonas";
+            this.spinnerNumeroDeDonas.Size = new System.Drawing.Size(120, 20);
+            this.spinnerNumeroDeDonas.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 462);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Número de donas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 495);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.spinnerNumeroDeDonas);
+            this.Controls.Add(this.btnAceptar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.spinnerNumeroDeDonas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.NumericUpDown spinnerNumeroDeDonas;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EjercicioDonas/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioDonas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            spinnerNumeroDeDonas.Minimum = 1;
+            spinnerNumeroDeDonas.Maximum = 1000;
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            int count = (int) spinnerNumeroDeDonas.Value;
+            Color colorPintura = Color.Blue;
+            SolidBrush linea = null;
+            Graphics g = CreateGraphics();
+            int x = 0;
+            int y = 0;
+            int w = 0;
+            int h = 0;
+            var ran = new Random();
+            g.Clear(Color.Gray);
+            Color[] colores = { Color.Red, Color.White, Color.Purple,
+                              Color.Yellow, Color.Green, Color.LightBlue,
+                              Color.LightCyan, Color.LightPink,
+                              Color.Pink, Color.Salmon, Color.SteelBlue};
+            int[] tamanyos = { 50, 60, 70, 80, 90, 100 };
+            for (int ix = 0; ix < count; ++ix)
+            {
+                x = ran.Next(0, Width);
+                y = ran.Next(0, Height);
+                w = tamanyos[ran.Next(0, tamanyos.Length - 1)];
+                h = tamanyos[ran.Next(0, tamanyos.Length - 1)];
+                Color c = colores[ran.Next(0, colores.Length)];
+                linea = new System.Drawing.SolidBrush(c);
+                g.FillEllipse(linea, x, y, w, h);
+                c = colores[ran.Next(0, colores.Length)];
+                linea = new System.Drawing.SolidBrush(Color.White);
+                g.FillEllipse(linea, x+5, y+5, w-10, h-10);                               
+            }
+        }
+    }
+}
